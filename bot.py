7ea9f9a29f13df9b51336e337b27da2494d9b3ff
spookyfiish <daothienphu1429@gmail.com>
2021-10-22 07:55:43 +0700

    hide config vars

--- bot.py
+++ bot.py
@@ -7,8 +7,7 @@ from dotenv import load_dotenv
 from discord.ext import commands
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-TOKEN += "e8tCMJ29xM-cixRXxk"
+TOKEN = os.environ['DISCORD_TOKEN']
 GUILD = int(os.getenv('DISCORD_GUILD'))
 
 bot = commands.Bot(command_prefix='.')
